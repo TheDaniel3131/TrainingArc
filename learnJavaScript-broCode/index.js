350436f1119f3263eab1d4514613d590caf37bf8
zephrous <danielpoh3131@gmail.com>
2024-01-03 19:53:18 +0800

    up.

--- learnJavaScript-broCode/index.js
+++ learnJavaScript-broCode/index.js
@@ -1,6 +1,37 @@
+// lesson 1
+
 // console.log(
 //     "Hello World!"
 // )
 
-document.getElementById("myH1").textContent = "Hello Daniel."
+/* 
+    MULTIPLE LINE COMMENTS
+*/
 
+// Lesson 2
+
+// document.getElementById("myH1").textContent = "Hello Daniel."
+// document.getElementById("myP1").textContent = "I am mania player."
+
+// Lesson 3
+
+// let x;
+// x = 5;
+// console.log(x);
+
+// Lesson 4
+
+console.log("I like pizza!");
+console.log("It's really good!");
+window.alert("I REALLY LOVE PIZZA!");
+
+//This is a comment
+/*
+    This
+    is
+    a
+    multiline
+    comment
+*/
+
+// Lesson 5
