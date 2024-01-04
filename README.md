42e74f0eb63624e3c33408f8d0170cf918d5eaf6
zephrous <danielpoh3131@gmail.com>
2024-01-04 15:59:01 +0800

    updated more lessons.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 "# TrainingArc" 
+
+Some parts of my folders and files are quite messy. The purpose of this trainingArc is to keep myself motivated and learn something in the process of learning new programming language out of it.
--- learnJavaScript-broCode/index.js
+++ learnJavaScript-broCode/index.js
@@ -609,3 +609,22 @@ for(let i = 1; i <= rows; i+=1){
 
 // Lesson 28
 
+//function = Define code once, and use it many times.
+//                   To perform some code, call the function name.
+
+startProgram();
+
+function startProgram(){
+    let userName = "Bro";
+    let age = 21;
+    
+    happyBirthday(userName, age);
+}
+
+function happyBirthday(userName, age){
+    console.log("Happy birthday to you!");
+    console.log("Happy birthday to you!");
+    console.log("Happy birthday dear", userName);
+    console.log("Happy birthday to you!");
+    console.log("You are", age,"years old!");
+}
