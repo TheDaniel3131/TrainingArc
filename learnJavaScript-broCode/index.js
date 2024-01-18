513e66ac53ce157a555923d3293e2f7383ca6164
zephrous <danielpoh3131@gmail.com>
2024-01-18 12:31:57 +0800

    50

--- learnJavaScript-broCode/index.js
+++ learnJavaScript-broCode/index.js
@@ -1174,3 +1174,28 @@ function shuffle(array){
   
   return array;
 }
+
+// Lesson 51
+
+// nested functions = Functions inside other functions.
+//                                   Outer functions have access to inner functions.
+//                                   Inner functions are "hidden" from outside.
+//                                   used in closures (future video topic)
+ 
+let userName = "Bro";
+let userInbox = 0;
+
+login();
+
+function login(){
+    displayUserName();
+    displayUserInbox();
+
+    function displayUserName(){
+        console.log(`Welcome ${userName}`);
+    }
+    function displayUserInbox(){
+        console.log(`You have ${userInbox} new messages`);
+    }
+}
+
