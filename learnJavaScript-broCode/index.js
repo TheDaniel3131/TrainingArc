28caa2f4e3e34b6a0880063ba2da832f507666ee
zephrous <danielpoh3131@gmail.com>
2024-01-24 14:48:42 +0800

    updated

--- learnJavaScript-broCode/index.js
+++ learnJavaScript-broCode/index.js
@@ -1199,3 +1199,59 @@ function login(){
     }
 }
 
+// Lesson 52
+
+// closure =  A function with preserved and private data.
+//                   Gives you access to an outer function’s scope,
+//                   from an inner function.
+
+document.getElementById("loginButton").onclick = login();
+
+//userInbox = 420.69;
+
+function login(){
+    let userName = "Bro";
+    let userInbox = 1;
+
+    function alertUser(){
+        alert(`${userName} you have ${userInbox} new messages!`);
+        userInbox = 0;
+    }
+
+    return alertUser;
+}
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Document</title>
+//     <link rel="stylesheet" href="style.css">
+// </head>
+// <body>
+//     <button id="loginButton">login</button>
+//     <script src="index.js"></script>
+// </body>
+// </html>
+
+
+// Lesson 53
+
+// Map = object that holds key-value pairs of any data type
+
+const store = new Map([
+  ["t-shirt", 20],
+  ["jeans", 30],
+  ["socks", 10],
+  ["underwear", 50]
+]);
+
+//store.get("t-shirt");
+//store.set("hat", 40);
+//store.delete("hat");
+//console.log(store.has("underwear"));
+//console.log(store.size);
+
+store.forEach((value, key) => console.log(`${key} $${value}`));
