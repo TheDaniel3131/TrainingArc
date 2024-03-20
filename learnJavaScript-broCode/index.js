4beb4a154cfa917d0402a6d649af7e489e7a5c4a
zephrous <danielpoh3131@gmail.com>
2024-03-20 20:25:46 +0800

    UPDATE

--- learnJavaScript-broCode/index.js
+++ learnJavaScript-broCode/index.js
@@ -1302,7 +1302,32 @@ car2.drive();
 car2.brake();
 
 
+// Lesson 55
 
+// this = reference to a particular object
+//           the object depends on the immediate context
+
+const car1 = {
+    model:"Mustang",
+    color:"red",
+    year:2023,
+
+    drive : function(){
+        console.log(`You drive the ${this.model}`);
+    }
+}
+const car2 = {
+    model:"Corvette",
+    color:"blue",
+    year:2024,
+
+    drive : function(){
+        console.log(`You drive the ${this.model}`);
+    }
+}
+
+car1.drive();
+car2.drive();
 
 
 
